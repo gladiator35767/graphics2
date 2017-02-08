be433227d3ce8cc8cf12900a5f7f91f79b7d995e
anthony.battista <anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-08 10:21:41 -0700

    Home Commit

---
+++ Graphics/src/Graphics215.java
@@ -0,0 +1,17 @@
+
+public class Graphics215
+	{
+		static int x = 100;
+		static int speed = 3; 
+
+		public static void main(String[] args)
+			{
+				
+				x = x + speed;
+				if(x == 100)
+					speed = -speed;
+				
+
+			}
+
+	}
--- Graphics/src/graphicsChallenge2.java
+++ Graphics/src/graphicsChallenge2.java
@@ -9,6 +9,8 @@ public class graphicsChallenge2 extends Canvas
 		static int Size;
 		static Scanner shapeColor;
 		static int setColor;
+		static int x = 100;
+		static int speed = 3;
 
 		public static void main(String[] args)
 			{
@@ -23,28 +25,39 @@ public class graphicsChallenge2 extends Canvas
 			}
 		public void paint(Graphics graphics)
 		{
-			for(int i = 0; i < 500; i++)
-				{
+			
+
+        while(true)
+        	{
+			x = x + speed;
+
 					graphics.setColor(Color.blue);
-					graphics.fillRect(i, i, 500, 500);
+					graphics.fillRect(x, 20, 20, 20);
+					
+					delay();
 					
 					graphics.setColor(Color.white);
-					graphics.fillRect(i, i, 500, 500);
-				}
-			for(int i = 0; i < 500; i++)
-				{
-					graphics.setColor(Color.red);
-					graphics.fillRect(i, i, 100, 100);
+					graphics.fillRect(x, 20, 20, 20);
 					
-					graphics.setColor(Color.white);
-					graphics.fillRect(i, i, 100, 100);
-				}
+			if(x > 970 || x < 0)
+				speed = -speed;
+			
+        	}	
+//			for(int i = 0; i < 500; i++)
+//				{
+//					graphics.setColor(Color.red);
+//					graphics.fillRect(i, i, 100, 100);
+//					
+//					graphics.setColor(Color.white);
+//					graphics.fillRect(i, i, 100, 100);
+//				}
+			
 		}
 		public void delay()
 		{
 			try
 				{
-					Thread.sleep(100);
+					Thread.sleep(1);
 				} catch (InterruptedException e)
 				{
 			    e.printStackTrace();
