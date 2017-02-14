0260b7345ecf5ef97be4676caaa495993ba01568
anthony.battista <anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-14 14:57:45 -0700

    Home commit

--- Graphics/src/graphicsChallenge2.java
+++ Graphics/src/graphicsChallenge2.java
@@ -11,10 +11,11 @@ public class graphicsChallenge2 extends Canvas
 		static String setColor;
 		static int x = 100;
 		static int speed = 3;
+		static int y = 0;
+		static int i = 0;
 
 		public static void main(String[] args)
 			{
-				scanner();
 				graphicsChallenge2 canvas = new graphicsChallenge2();
 		        JFrame frame = new JFrame();
 		        frame.setSize(1000, 1000);
@@ -24,54 +25,56 @@ public class graphicsChallenge2 extends Canvas
 		        frame.setResizable(true);
 		        frame.setVisible(true);	
 			}
-		public static void scanner()
+		public void paint(Graphics graphics)
 		{
-			Square = new Scanner(System.in);
+			Scanner userInput = new Scanner (System.in);
 			System.out.println("What would you like the size of"
 					+ " the your square to be?(Maximum 999)");
 			shapeColor = new Scanner(System.in);
-			Size = shapeColor.nextInt();
+			Size = userInput.nextInt();
 			
-			Square = new Scanner(System.in);
+			Scanner userInput2 = new Scanner(System.in);
 			System.out.println("Between red and blue which color would"
 					+ " you like your square to be? (red or blue)");
-			
-			setColor = shapeColor.nextLine();
-		}
-		public void one(Graphics graphics)
-		{
-			if(setColor.equals("red"))
+			setColor = userInput2.nextLine();
+		
+		
+			if(userInput2.equals("red"))
 				{
 					graphics.setColor(Color.red);
 					graphics.fillRect(0, 0, Size, Size);
+					
 				}
-			if(setColor.equals("blue"))
+			if(userInput2.equals("blue"))
 				{
 					graphics.setColor(Color.blue);
 					graphics.fillRect(0, 0, Size, Size);
 				}
-			
-		}
+	}
 		
 		
-		public void paint(Graphics graphics)
-		{
-        while(true)
-        	{
-			x = x + speed;
-
-					graphics.setColor(Color.blue);
-					graphics.fillRect(x, 20, 20, 20);
-					
-					delay();
-					
-					graphics.setColor(Color.white);
-					graphics.fillRect(x, 20, 20, 20);
-					
-			if(x > 980 || x < 0)
-				speed = -speed;
-			
-        	}	
+		
+		
+		
+//		public void paint(Graphics graphics)
+//		{
+//        while(true)
+//        	{
+//			x = x + speed;
+//
+//					graphics.setColor(Color.blue);
+//					graphics.fillRect(x, 20, 20, 20);
+//					
+//					delay();
+//					
+//					graphics.setColor(Color.white);
+//					graphics.fillRect(x, 20, 20, 20);
+//					
+//			if(x > 980 || x < 0)
+//				speed = -speed;
+//			
+//        	}
+        
 //			for(int i = 0; i < 500; i++)
 //				{
 //					graphics.setColor(Color.red);
@@ -81,7 +84,7 @@ public class graphicsChallenge2 extends Canvas
 //					graphics.fillRect(i, i, 100, 100);
 //				}
 			
-		}
+//		}
 		public void delay()
 		{
 			try
