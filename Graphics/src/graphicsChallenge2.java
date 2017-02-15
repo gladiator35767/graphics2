d7aed88e411b09248587774137f7b1de4b691413
Anthony.battista <Anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-15 15:30:40 -0700

    Day 2 Commit

--- Graphics/src/graphicsChallenge2.java
+++ Graphics/src/graphicsChallenge2.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class graphicsChallenge2 extends Canvas
 	{
 		private static final long serialVersionUID = 1L;
-		static Scanner Square;
+		static Scanner userInput;
 		static int Size;
 		static Scanner shapeColor;
 		static String setColor;
@@ -16,6 +16,7 @@ public class graphicsChallenge2 extends Canvas
 
 		public static void main(String[] args)
 			{
+//				takeInput();
 				graphicsChallenge2 canvas = new graphicsChallenge2();
 		        JFrame frame = new JFrame();
 		        frame.setSize(1000, 1000);
@@ -25,32 +26,67 @@ public class graphicsChallenge2 extends Canvas
 		        frame.setResizable(true);
 		        frame.setVisible(true);	
 			}
+//		public static void takeInput()
+//			{
+//				Scanner userInput = new Scanner (System.in);
+//				System.out.println("What would you like the size of"
+//						+ " the your square to be?(Maximum 999)");
+//				shapeColor = new Scanner(System.in);
+//				Size = userInput.nextInt();
+//				
+//				Scanner userInput2 = new Scanner(System.in);
+//				System.out.println("Between red and blue which color would"
+//						+ " you like your square to be? (red or blue)");
+//				setColor = userInput2.nextLine();
+//			}
+//		
+//		public void paint(Graphics graphics)
+//		{
+//			if(setColor.equals("red"))
+//				{
+//					graphics.setColor(Color.red);
+//					graphics.fillRect(0, 0, Size, Size);
+//					
+//				}
+//			if(setColor.equals("blue"))
+//				{
+//					graphics.setColor(Color.blue);
+//					graphics.fillRect(0, 0, Size, Size);
+//				}
+//	    }
+		
+//		public void paint(Graphics graphics)
+//		{
+//			for (int r = 0; r < 255; r++)
+//				{
+//					for (int g = 0; g < 255; g++)
+//						{
+//							for (int b = 0; b < 255; b++)
+//								{
+//									graphics.setColor(new Color(r, g, b));
+//									graphics.fillRect(0, 0, 100, 100);
+//									delay();
+//								}
+//						}
+//				}
+//		}
 		public void paint(Graphics graphics)
 		{
-			Scanner userInput = new Scanner (System.in);
-			System.out.println("What would you like the size of"
-					+ " the your square to be?(Maximum 999)");
-			shapeColor = new Scanner(System.in);
-			Size = userInput.nextInt();
-			
-			Scanner userInput2 = new Scanner(System.in);
-			System.out.println("Between red and blue which color would"
-					+ " you like your square to be? (red or blue)");
-			setColor = userInput2.nextLine();
 		
-		
-			if(userInput2.equals("red"))
-				{
-					graphics.setColor(Color.red);
-					graphics.fillRect(0, 0, Size, Size);
-					
-				}
-			if(userInput2.equals("blue"))
-				{
-					graphics.setColor(Color.blue);
-					graphics.fillRect(0, 0, Size, Size);
-				}
-	}
+        for (int i = 0; i < 350; i++)
+        	{
+        		for (int j = 0; j <500; j++)
+        			{
+	        graphics.setColor(Color.blue);
+	        graphics.fillRect(j, i, 50, 50);
+	        
+	        delay();
+	        
+	        graphics.setColor(Color.white);
+	        graphics.fillRect(j, i, 50, 50);
+        			}
+        	}
+		}
 		
 		
 		
